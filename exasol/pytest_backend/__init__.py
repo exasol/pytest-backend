86a3ab8d7c1a380fceb60178e971f6dedd17c6f4
Ariel Schulz <ariel.schulz@exasol.com>
2026-04-29 11:57:07 +0200

    Run format:fix

--- exasol/pytest_backend/__init__.py
+++ exasol/pytest_backend/__init__.py
@@ -4,6 +4,7 @@ import os
 import ssl
 from collections.abc import Iterable
 from datetime import timedelta
+from importlib.metadata import version
 from typing import (
     Any,
 )
@@ -33,7 +34,6 @@ from .itde import (
 )
 from .parallel_task import paralleltask
 
-from importlib.metadata import version
 __version__ = version("pytest-exasol-backend")
 
 BACKEND_OPTION = "--backend"
