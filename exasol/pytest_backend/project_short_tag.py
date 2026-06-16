2f1182c173554303c8e1ecd8652b1e0312d4e4a3
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-06-16 09:44:20 +0200

    Fixed sonar warning: Remove the unused local variable "ex".
    Signed-off-by: Steffen Pankratz <steffen.pankratz@exasol.com>

--- exasol/pytest_backend/project_short_tag.py
+++ exasol/pytest_backend/project_short_tag.py
@@ -55,7 +55,7 @@ def read_from_yaml(start_dir: Path) -> str | None:
         ecc = yaml.safe_load(file)
         try:
             return next(t for t in ecc["error-tags"])
-        except Exception as ex:
+        except Exception:
             raise RuntimeError(
                 f"Could not read project short tag from file {config_file}"
             )
